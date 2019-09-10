8db0b07a4204810fac92e9ed19b6600af3f771b0
al <alfredo.yap@stu.bmcc.cuny.edu>
2019-09-10 14:42:31 -0400

    2) App Structure - Finished

--- app/src/main/java/com/example/tinderclone/activities/TinderActivity.kt
+++ app/src/main/java/com/example/tinderclone/activities/TinderActivity.kt
@@ -6,23 +6,92 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.ArrayAdapter
+import android.widget.TableLayout
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.example.tinderclone.R
+import com.example.tinderclone.fragments.MatchesFragment
+import com.example.tinderclone.fragments.ProfileFragment
+import com.example.tinderclone.fragments.SwipeFragment
+import com.google.android.material.tabs.TabLayout
 import com.lorentzos.flingswipe.SwipeFlingAdapterView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class TinderActivity : AppCompatActivity() {
 
 
+    private var profileFragment: ProfileFragment? = null
+    private var swipeFragment: SwipeFragment? = null
+    private var matchesFragment: MatchesFragment? = null
+
+    private var profileTab: TabLayout.Tab? = null
+    private var swipeTab: TabLayout.Tab? = null
+    private var matchesTab: TabLayout.Tab? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Creating the navigation tabs
+        profileTab = navigationTabs.newTab()
+        swipeTab = navigationTabs.newTab()
+        matchesTab = navigationTabs.newTab()
+
+        //Assigning icons to the each tab
+        profileTab?.icon = ContextCompat.getDrawable(this, R.drawable.tab_profile)
+        swipeTab?.icon = ContextCompat.getDrawable(this, R.drawable.tab_swipe)
+        matchesTab?.icon = ContextCompat.getDrawable(this, R.drawable.tab_matches)
+
+        navigationTabs.addTab(profileTab!!)
+        navigationTabs.addTab(swipeTab!!)
+        navigationTabs.addTab(matchesTab!!)
+
+        navigationTabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+                onTabSelected(tab)
+            }
+
+            override fun onTabUnselected(p0: TabLayout.Tab?) {
+                // keep this blank
+            }
+
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab) {
+                    profileTab -> {
+                        if (profileFragment == null) {
+                            profileFragment = ProfileFragment()
+                        }
+                        replaceFragment(profileFragment!!)
+                    }
+                    swipeTab -> {
+                        if (swipeFragment == null) {
+                            swipeFragment = SwipeFragment()
+                        }
+                        replaceFragment(swipeFragment!!)
+                    }
+                    matchesTab -> {
+                        if (matchesFragment == null) {
+                            matchesFragment = MatchesFragment()
+                        }
+                        replaceFragment(matchesFragment!!)
+                    }
+                }
+            }
+
+        })
+        profileTab?.select()
     }
 
     //Static function in Kotlin
-    companion object{
+    companion object {
         fun newIntent(context: Context?) = Intent(context, TinderActivity::class.java)
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
