a6004895d9aa5532212d973635b24199a755cc2f
al <alfredo.yap@stu.bmcc.cuny.edu>
2019-09-11 13:41:08 -0400

    4) Swiping - Swipe Tab Functionality

--- app/src/main/java/com/example/tinderclone/fragments/SwipeFragment.kt
+++ app/src/main/java/com/example/tinderclone/fragments/SwipeFragment.kt
@@ -6,9 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 
 import com.example.tinderclone.R
 import com.example.tinderclone.activities.TinderCallback
+import com.example.tinderclone.util.User
+import com.google.firebase.database.DatabaseReference
 
 /**
  * A simple [Fragment] subclass.
@@ -16,9 +19,15 @@ import com.example.tinderclone.activities.TinderCallback
 class SwipeFragment : Fragment() {
 
     private var callback: TinderCallback? = null
+    private lateinit var  userId: String
+    private lateinit var userDatabase: DatabaseReference
+    private var cardsAdapter : ArrayAdapter<User>? = null
+    private var rowItems  = ArrayList<User>()
 
     fun setCallback(callback: TinderCallback) {
         this.callback = callback
+        userId = callback.onGetUserId()
+        userDatabase = callback.getDatabase()
     }
 
 
@@ -28,6 +37,11 @@ class SwipeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_swipe, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+//        cardsAdapter =
     }
 
 
