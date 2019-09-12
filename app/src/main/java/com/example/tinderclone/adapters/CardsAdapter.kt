0656b78e103c3d7a38691165b38c54a68a9f815e
al <alfredo.yap@stu.bmcc.cuny.edu>
2019-09-11 20:36:17 -0400

    4) Swiping - Finished

--- app/src/main/java/com/example/tinderclone/adapters/CardsAdapter.kt
+++ app/src/main/java/com/example/tinderclone/adapters/CardsAdapter.kt
@@ -6,9 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.example.tinderclone.R
+import com.example.tinderclone.activities.UserInfoActivity
 import com.example.tinderclone.util.User
 
 
@@ -20,16 +22,16 @@ class CardsAdapter(context: Context, resourceId: Int, users: List<User>): ArrayA
 
         var name = finalView.findViewById<TextView>(R.id.nameTV)
         var image = finalView.findViewById<ImageView>(R.id.photoIV)
-      //  var userInfo = finalView.findViewById<LinearLayout>(R.id.userInfoLayout)
+      var userInfo = finalView.findViewById<LinearLayout>(R.id.userInfoLayout)
 
         name.text = "${user?.name}, ${user?.age}"
         Glide.with(context)
             .load(user?.imageUrl)
             .into(image)
-//
-//        userInfo.setOnClickListener {
-//            finalView.context.startActivity(UserInfoActivity.newIntent(finalView.context, user.uid))
-//        }
+
+        userInfo.setOnClickListener {
+            finalView.context.startActivity(UserInfoActivity.newIntent(finalView.context, user?.uid))
+        }
 
         return finalView
     }
